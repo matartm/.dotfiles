80e1790b2005881d322315c605e6fc47fd763df2
matartm <matthias.artmeyer@proton.me>
2025-07-12 22:18:04 +0200

    hyprland updates

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>s" , ":source $HOME/.config/nvim/init.lua <CR>")
+vim.keymap.set("n", "<leader>s" , "<cmd>source $HOME/.config/nvim/init.lua<cr>")
+vim.keymap.set("n", "<leader>w" , "<cmd>wincmd w<cr>")
+
