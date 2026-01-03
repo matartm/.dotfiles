3a46e71b1a793b14d89a71f6168d4d822bce1ef4
Athemes <matthias.artmeyer@proton.me>
2026-01-03 21:54:11 +0100

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,11 @@ alias e="nvim"
 alias vim="nvim"
 alias view="nvim -R"
 alias get_idf=". $HOME/projects/esp/esp-idf55/export.fish"
-keychain --eval --quiet ~/security/ssh/git_ed25519 | source
+#keychain --eval ~/.ssh/id_ed25519 | source
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	if not set -q SSH_AUTH_SOCK
+		eval (ssh-agent -c)
+	end
+    ssh-add ~/.ssh/id_ed25519 2>/dev/null
 end
